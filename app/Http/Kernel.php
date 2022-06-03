ad5e3ad351e195d466c566837782e4c7d68acfe5
jltkd <jl@turnkeydigital.com>
2022-06-03 09:42:47 -0500

    Installed Inertia

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -4,7 +4,7 @@ namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
-class Kernel extends HttpKernel
+class   Kernel extends HttpKernel
 {
     /**
      * The application's global HTTP middleware stack.
@@ -36,6 +36,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\HandleInertiaRequests::class,
         ],
 
         'api' => [
